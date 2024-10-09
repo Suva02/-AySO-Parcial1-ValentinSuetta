853f5e35587dd4fe3d3dd695188b181d1d3e8f7b
Suva02 <suettavalentin2@gmail.com>
2024-10-09 02:27:19 +0000

    Puntos del parcial

--- puntoe.sh
+++ puntoe.sh
@@ -1,3 +1,3 @@
 cd AySO-Parcial1-ValentinSuetta/
 grep MemTotal /proc/meminfo > Filtro_Basico.txt
-sudo dmidecode -t chassis | grep Manufacturer >> Filtro_Basico.txt
+sudo dmidecode -t chassis | grep Manufacturer | sed '1i Chassis information' >> Filtro_Basico.txt
